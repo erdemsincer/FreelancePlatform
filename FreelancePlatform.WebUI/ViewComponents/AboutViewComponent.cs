fc3d5f6d93be05c7a131f6bd89d0ffcdffe973b9
Erdem <erdemsincer123@icloud.com>
2025-03-23 04:02:00 +0300

    about view component

---
+++ FreelancePlatform.WebUI/ViewComponents/AboutViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FreelancePlatform.WebUI.ViewComponents
+{
+    public class AboutViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+          
+            return View();
+        }
+    }
+}
