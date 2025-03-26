8042c0c299e6f38b256c55eec5e2f6a51b8c5c6b
Erdem <erdemsincer123@icloud.com>
2025-03-27 00:40:38 +0300

    Freelasncer layout

---
+++ FreelancePlatform.WebUI/Areas/Freelancer/Controllers/FreelancerLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FreelancePlatform.WebUI.Areas.Freelancer.Controllers
+{
+    public class FreelancerLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
