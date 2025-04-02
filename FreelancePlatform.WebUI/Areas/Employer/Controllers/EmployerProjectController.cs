c9d7459c3ec4193cae89faa47b9c78e309c65a8e
Erdem <erdemsincer123@icloud.com>
2025-04-02 16:21:10 +0300

    employer tüm projeler

---
+++ FreelancePlatform.WebUI/Areas/Employer/Controllers/EmployerProjectController.cs
@@ -0,0 +1,40 @@
+﻿using FreelancePlatform.Core.DTOs.ProjectDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace FreelancePlatform.WebUI.Areas.Employer.Controllers
+{
+    [Area("Employer")]
+    public class EmployerProjectController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public EmployerProjectController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> MyProjects()
+        {
+            var userId = HttpContext.Session.GetInt32("userId");
+            var token = HttpContext.Session.GetString("token");
+
+            if (userId == null || string.IsNullOrEmpty(token))
+                return RedirectToAction("Login", "Auth");
+
+            var client = _httpClientFactory.CreateClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync($"https://localhost:7085/api/Project/employer/{userId}");
+
+            if (!response.IsSuccessStatusCode)
+                return View(new List<ResultProjectDto>());
+
+            var json = await response.Content.ReadAsStringAsync();
+            var projects = JsonConvert.DeserializeObject<List<ResultProjectDto>>(json);
+
+            return View(projects);
+        }
+    }
+}
