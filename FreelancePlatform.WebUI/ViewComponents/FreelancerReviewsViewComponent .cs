33014d2ab871f03fdc0d841e6119f2003721e826
Erdem <erdemsincer123@icloud.com>
2025-04-03 17:40:50 +0300

    frelancer yorumlar

---
+++ FreelancePlatform.WebUI/ViewComponents/FreelancerReviewsViewComponent .cs
@@ -0,0 +1,30 @@
+﻿using FreelancePlatform.Core.DTOs.ReviewDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FreelancePlatform.WebUI.ViewComponents
+{
+    public class FreelancerReviewsViewComponent : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public FreelancerReviewsViewComponent(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int userId)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7085/api/Review/freelancer/{userId}");
+
+            if (!response.IsSuccessStatusCode)
+                return View(new List<ResultReviewDto>());
+
+            var json = await response.Content.ReadAsStringAsync();
+            var reviews = JsonConvert.DeserializeObject<List<ResultReviewDto>>(json);
+
+            return View(reviews);
+        }
+    }
+}
