a65f9a8391b725f7cd00611050191cd5259abf84
Erdem <erdemsincer123@icloud.com>
2025-04-03 17:40:38 +0300

    profil için user bilgilerini gösteren component

---
+++ FreelancePlatform.WebUI/ViewComponents/FreelancerProfileInfoViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using FreelancePlatform.Core.DTOs.UserDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FreelancePlatform.WebUI.ViewComponents
+{
+    public class FreelancerProfileInfoViewComponent : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public FreelancerProfileInfoViewComponent(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int freelancerId)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7085/api/User/public/{freelancerId}");
+
+            if (!response.IsSuccessStatusCode)
+                return View(null); // veya hata view'ı döndür
+
+            var json = await response.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<ResultUserDto>(json);
+
+            return View(user);
+        }
+    }
+}
