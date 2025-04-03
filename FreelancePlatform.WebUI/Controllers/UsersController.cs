788144174b17ada6fbcb3b2d402ccfd19a977c3e
Erdem <erdemsincer123@icloud.com>
2025-04-03 17:41:22 +0300

    User profil sayfası

---
+++ FreelancePlatform.WebUI/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using FreelancePlatform.Core.DTOs.UserDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreelancePlatform.WebUI.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public UsersController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task< IActionResult> Index(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var values=await client.GetFromJsonAsync<ResultUserDto>("https://localhost:7085/api/User/" + id);
+            ViewBag.UserId = id;
+            return View(values);
+        }
+    }
+}
